3c19359e2906177a9adf2bc5523e2d3c63d55b76
Ch4rle5M <vilux321@gmail.com>
2025-09-02 13:08:00 +0700

    feat : fix this vro

---
+++ aboutus.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <h1>pretend this as about us Bruh</h1>
+    <table>
+        <li>
+            Bruh
+        </li>
+
+        <li>
+
+            yes
+
+        </li>
+    </table>    
+
+</body>
+</html>
